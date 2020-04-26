f6b2fb077ed29c89c899e48d6446ff8720f608f5
Pedro H. Victorino <pvictorino.dev@gmail.com>
2020-04-26 13:24:09 -0400

    continuando desenvolvimento inicial

---
+++ src/components/Profile/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Avatar from "../Avatar"
+
+import * as S from "./styled"
+
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          position
+        }
+      }
+    }
+  `)
+
+  return (
+    <S.ProfileWrapper>
+      <S.ProfileLink>
+        <Avatar />
+        <S.ProfileAuthor>
+          {title}
+          <S.ProfilePosition>{position}</S.ProfilePosition>
+        </S.ProfileAuthor>
+      </S.ProfileLink>
+      <S.ProfileDescription>{description}</S.ProfileDescription>
+    </S.ProfileWrapper>
+  )
+}
+
+export default Profile
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,12 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/phvictorino'
+  },
+  {
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/pedro-henrique-victorino-a7854ab3/'
+  }
+]
+
+export { links }
