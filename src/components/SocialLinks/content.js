4c95e9ec5343cc2c2708cbdcfb6507097af14d46
Pedro H. Victorino <pvictorino.dev@gmail.com>
2020-04-26 18:27:12 -0400

    continuando desenvolvimento inicial

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -1,12 +1,10 @@
-const links = [
+export const links = [
   {
     label: 'Github',
     url: 'https://github.com/phvictorino'
   },
   {
-    label: 'LinkedIn',
+    label: 'Linkedin',
     url: 'https://www.linkedin.com/in/pedro-henrique-victorino-a7854ab3/'
   }
 ]
-
-export { links }
